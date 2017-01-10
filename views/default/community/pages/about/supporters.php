e2c4aab610bd48bbf1a9c80b4406e9aaa8e5026c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-01-10 13:38:21 +0100

    feat(pages): get rid of ugly tables from supporters page

--- views/default/community/pages/about/supporters.php
+++ views/default/community/pages/about/supporters.php
@@ -1,19 +1,19 @@
-<p>
+<p class="elgg-output">
 	Are you an individual or organization who uses and likes Elgg? Consider becoming an official supporter of the Elgg project.
 	With only $50 per year for individuals or $150 per year for organizations, you can help the Elgg project.
 </p>
-<p>
+<p class="elgg-output">
 	Elgg supporters are listed below unless requested not to be. If you would like to become an Elgg supporter, please read the <a href="#disclaimer">disclaimer</a>,
 	subscribe via PayPal,
 	and send an email to <a	href="mailto:info@elgg.org">info@elgg.org</a> confirming the date you subscribed and the relevant details for your level
 	(these can be found on the tables below). Once all the details have been received, we will add you to the appropriate list. Thanks for your support!
 </p>
-<p>
+<p class="elgg-output">
 	All funds raised via the Elgg supporters network go directly into core Elgg development and  provision (elgg.org/svn/trac etc).
 	It is a great way to help with Elgg development!
 </p>
 
-<p>
+<p class="elgg-output">
 	Supporters are able to put this official logo on their site if they wish:
 <div>
 	<?=
@@ -25,230 +25,292 @@
 </div>
 </p>
 
+<?php
+$organizations = [
+	[
+		'name' => 'Irene Ilina',
+		'organization' => 'SkaDate Dating Software LLC',
+		'url' => 'http://www.skadate.com',
+	],
+	[
+		'name' => 'Fabrice Collette',
+		'organization' => 'Mélèze Conseil',
+		'url' => 'http://www.meleze-conseil.com',
+		'username' => 'fabcol',
+	],
+	[
+		'name' => 'Paul Stewart',
+		'organization' => 'Arck Interactive LLC',
+		'url' => 'http://arckinteractive.com',
+		'username' => 'arckinteractive',
+	],
+	[
+		'name' => 'Caroline Meeks',
+		'organization' => 'Solution Grove',
+		'url' => 'http://www.solutiongrove.com/xowiki/elgg',
+	],
+	[
+		'name' => 'Jeroen Dalsem',
+		'organization' => 'ColdTrick IT Solutions',
+		'url' => 'http://www.coldtrick.com',
+		'username' => 'jdalsem',
+	],
+	[
+		'name' => 'Tom Voorneveld',
+		'url' => 'http://www.lorinthe.com',
+		'organization' => Lorinthe,
+	], [
+		'name' => 'Mike Kasper',
+		'url' => 'http://myfreepersonals.com/',
+		'organization' => 'My Free Personals',
+		'username' => 'topdog08',
+	],
+	[
+		'name' => 'Andrew Boon',
+		'url' => 'http://www.boonex.com',
+		'organization' => 'BoonEx Community Software',
+	],
+	[
+		'name' => 'Veronika Cardes',
+		'url' => 'http://www.dating-service.com',
+		'nofollow' => true,
+		'organization' => 'Dating Service.com',
+	],
+	[
+		'name' => 'Taka Fujita',
+		'organization' => 'Kototoy Factory Co., Ltd.',
+		'url' => 'http://kototoy.jp',
+		'username' => 'teilnehmen',
+	],
+	[
+		'name' => 'Jon Dron',
+		'organization' => 'Athabasca University',
+		'url' => 'http://athabascau.ca',
+	],
+	[
+		'name' => 'Teodor Nica',
+		'organization' => 'VideoWhisper Webcam Software',
+		'url' => '	http://www.videowhisper.com',
+		'username' => 'videowhisper',
+	],
+	[
+		'name' => 'Aron West',
+		'organization' => 'Compassion And Peace Association of Canada (CAPAOC)',
+		'url' => '	http://www.capaoc.org/',
+		'username' => 'aronw9376',
+	],
+	[
+		'name' => 'Michele & Gianni',
+		'organization' => 'Social Business World',
+		'url' => 'http://www.socialbusinessworld.org/',
+		'username' => 'michele',
+	],
+	[
+		'name' => 'HRDNZ (Moodle Partner)',
+		'organization' => 'Human Resource Development International Ltd',
+		'url' => 'http://www.hrdnz.com',
+		'username' => 'stuartrmealor',
+	],
+	[
+		'name' => 'Christophe Lamm',
+		'organization' => 'Ligne Bleue Cyber',
+		'url' => 'http://www.ligne-bleue-cyber.com/services/creation_de_sites_web.html',
+		'username' => 'clamm',
+	],
+	[
+		'name' => 'Elena Ganenkova',
+		'organization' => 'JetBrains',
+		'url' => 'http://www.jetbrains.com/',
+	],
+	[
+		'name' => 'Christian Bernert',
+		'organization' => 'Sogln Ltd.',
+		'url' => 'http://www.sogln.com/',
+		'username' => 'sogln',
+	],
+];
 
+usort($organizations, function($a, $b) {
+	return strcmp($a['organization'], $b['organization']);
+});
+
+$persons = [
+	[
+		'name' => 'Ben Werdmuller',
+		'url' => 'http://benwerd.com/',
+		'username' => 'bwerdmuller',
+	],
+	[
+		'name' => 'Marcus Povey',
+		'url' => 'http://www.marcus-povey.co.uk/',
+		'username' => 'marcus',
+	],
+	[
+		'name' => 'Mark Bridges',
+		'url' => 'http://www.activityseeker.com/',
+		'username' => 'MisterBridges',
+	],
+	[
+		'name' => 'Per Jensen',
+		'url' => 'http://www.perjensen-online.dk/',
+		'username' => 'gillie',
+	],
+	[
+		'name' => 'Dhrup Chand',
+		'url' => 'http://fkkids.com/',
+		'username' => 'Dhrup2000',
+	],
+	[
+		'name' => 'Torsten Wesolek',
+		'url' => 'http://www.community-software-24.com/',
+		'username' => 'westor',
+	],
+	[
+		'name' => 'Tarun Jangra',
+		'url' => 'http://www.pluginlotto.com/',
+		'username' => 'izap',
+	],
+	[
+		'name' => 'Shyam Somanadh',
+		'url' => 'http://frontiernxt.com/',
+		'username' => 'codelust',
+	],
+	[
+		'name' => 'John L. Spears',
+		'url' => 'http://www.clothingric.com/',
+		'username' => '',
+	],
+	[
+		'name' => 'Xavier Vaucois',
+		'url' => 'http://www.fairbusinessdeal.com/fbd',
+		'username' => '',
+	],
+	[
+		'name' => 'Gordon Wu',
+		'url' => 'http://unixpro.us/',
+		'username' => 'gordonwu888',
+	],
+];
+
+usort($persons, function($a, $b) {
+	return strcmp($a['name'], $b['name']);
+});
+?>
 <div class="elgg-module elgg-module-info">
 	<div class="elgg-head">
 		<h3>Organization Supporters</h3>
 	</div>
 	<div class="elgg-body">
-		<table class="elgg-table-alt">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Organization URL</th>
-					<th>Organization Name</th>
-					<th>Community Profile</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Irene Ilina</td>
-					<td><a href="http://www.skadate.com" rel="nofollow">http://www.skadate.com</a></td>
-					<td><a href="http://www.skadate.com" rel="nofollow">SkaDate Dating Software LLC</a></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Fabrice Collette</td>
-					<td><a href="http://www.meleze-conseil.com">http://www.meleze-conseil.com</a></td>
-					<td><a href="http://www.meleze-conseil.com">M&#233;l&#232;ze Conseil</a></td>
-					<td><a href="https://elgg.org/profile/fabcol">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Paul Stewart</td>
-					<td><a href="http://arckinteractive.com">http://arckinteractive.com</a></td>
-					<td><a href="http://arckinteractive.com">Arck Interactive LLC</a></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Caroline Meeks</td>
-					<td><a href="http://www.solutiongrove.com/xowiki/elgg">http://www.solutiongrove.com/xowiki/elgg</a></td>
-					<td><a href="http://www.solutiongrove.com/xowiki/elgg">Solution Grove</a></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Jeroen Dalsem</td>
-					<td><a href="http://www.coldtrick.com">http://www.coldtrick.com</a></td>
-					<td><a href="http://www.coldtrick.com">ColdTrick IT Solutions</a></td>
-					<td><a href="https://elgg.org/profile/jdalsem">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Tom Voorneveld</td>
-					<td><a href="http://www.lorinthe.com">http://www.lorinthe.com</a></td>
-					<td><a href="http://www.lorinthe.com">Lorinthe</a></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Mike Kasper</td>
-					<td><a href="http://myfreepersonals.com">http://myfreepersonals.com</a></td>
-					<td><a href="http://myfreepersonals.com">My Free Personals</a></td>
-					<td><a href="https://elgg.org/profile/topdog08">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Andrew Boon</td>
-					<td><a href="http://www.boonex.com">http://www.boonex.com</a></td>
-					<td><a href="http://www.boonex.com">BoonEx Community Software</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>Veronika Cardes</td>
-					<td><a href="http://www.dating-service.com" rel="nofollow">http://www.dating-service.com</a></td>
-					<td><a href="http://www.dating-service.com" rel="nofollow">Dating Service.com</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>Taka Fujita</td>
-					<td><a href="http://kototoy.jp/">http://kototoy.jp</a></td>
-					<td><a href="http://kototoy.jp">Kototoy Factory Co., Ltd.</a></td>
-					<td><a href="https://elgg.org/profile/teilnehmen">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Jon Dron</td>
-					<td><a href="http://athabascau.ca">http://athabascau.ca</a></td>
-					<td><a href="http://athabascau.ca">Athabasca University</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>Teodor Nica</td>
-					<td><a href="http://www.videowhisper.com">http://www.videowhisper.com</a></td>
-					<td><a href="http://www.videowhisper.com">VideoWhisper Webcam Software</a></td>
-					<td><a href="https://elgg.org/profile/videowhisper">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Aron West</td>
-					<td><a href="http://www.capaoc.org/" rel="nofollow">http://www.capaoc.org/</a></td>
-					<td><a href="http://www.capaoc.org/" rel="nofollow">Compassion And Peace Association of Canada
-							(CAPAOC)</a></td>
-					<td><a href="https://elgg.org/profile/aronw9376">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Michele &amp; Gianni</td>
-					<td><a href="http://www.socialbusinessworld.org/">http://www.socialbusinessworld.org/</a></td>
-					<td><a href="http://www.socialbusinessworld.org/">Organization Name Social Business World</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>HRDNZ (Moodle Partner)</td>
-					<td><a href="http://www.hrdnz.com">http://www.hrdnz.com</a></td>
-					<td><a href="http://www.hrdnz.com">Human Resource Development International Ltd</a></td>
-					<td><a href="https://elgg.org/profile/stuartrmealor">Visit</a></td>
-				</tr>
-				<tr>
-					<td>christophe lamm</td>
-					<td><a href="http://www.ligne-bleue-cyber.com/services/creation_de_sites_web.html">http://www.ligne-bleue-cyber.com/services/creation_de_sites_web.html</a>
-					</td>
-					<td><a href="http://www.ligne-bleue-cyber.com/services/creation_de_sites_web.html">ligne bleue cyber</a>
-					</td>
-					<td><a href="https://elgg.org/profile/clamm">visit</a></td>
-				</tr>
-				<tr>
-					<td>Elena Ganenkova</td>
-					<td><a href="http://www.jetbrains.com">http://www.jetbrains.com</a></td>
-					<td><a href="http://www.jetbrains.com">JetBrains</a></td>
-					<td><a href="http://www.jetbrains.com">visit</a></td>
-				</tr>
-				<tr>
-					<td>Christian Bernert</td>
-					<td><a href="http://www.sogln.com">http://www.sogln.com</a></td>
-					<td><a href="http://www.sogln.com">Sogln Ltd.</a></td>
-					<td><a href="https://elgg.org/profile/sogln">visit</a></td>
-				</tr>
-			</tbody>
-		</table>
+		<ul class="elgg-list">
+			<?php
+			foreach ($organizations as $organization) {
+				?>
+				<li class="elgg-item">
+					<h3><?= $organization['organization'] ?></h3>
+					<div class="elgg-subtext">
+						<?= $organization['name'] ?>
+					</div>
+					<div class="elgg-subtext">
+						<?php
+						echo elgg_view('output/url', [
+							'href' => $organization['url'],
+							'is_trusted' => !elgg_extract('nofollow', $organization, false),
+							'target' => '_blank',
+						]);
+						?>
+					</div>
+
+					<?php
+					$username = elgg_extract('username', $organization);
+					if (!$username) {
+						continue;
+					}
+					$user = get_user_by_username($username);
+					if ($user) {
+						$icon = elgg_view_entity_icon($user, 'topbar');
+						$link = elgg_view('output/url', [
+							'href' => $user->getURL(),
+							'text' => $user->username,
+						]);
+						echo elgg_view_image_block($icon, $link, [
+							'class' => 'elgg-subtext',
+						]);
+					}
+					?>
+				</li>
+				<?php
+			}
+			?>
+
+		</ul>
 	</div>
 </div>
+
 <div class="elgg-module elgg-module-info">
 	<div class="elgg-head">
 		<h3>Individual Supporters</h3>
 	</div>
 	<div class="elgg-body">
-		<table class="elgg-table-alt">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Website</th>
-					<th>Community Profile</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Ben Werdmuller</td>
-					<td><a href="http://benwerd.com">http://benwerd.com</a></td>
-					<td><a href="https://elgg.org/profile/bwerdmuller">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Marcus Povey</td>
-					<td><a href="http://www.marcus-povey.co.uk">http://www.marcus-povey.co.uk</a></td>
-					<td><a href="https://elgg.org/profile/marcus">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Mark Bridges</td>
-					<td><a href="http://www.activityseeker.com">www.activityseeker.com</a></td>
-					<td><a href="https://elgg.org/profile/MisterBridges">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Per Jensen</td>
-					<td><a href="http://www.perjensen-online.dk/">http://www.perjensen-online.dk/</a></td>
-					<td><a href="https://elgg.org/profile/gillie">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Dhrup Chand</td>
-					<td><a href="http://FKKids.Com">FKKids.Com</a></td>
-					<td><a href="https://elgg.org/profile/Dhrup2000">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Torsten Wesolek</td>
-					<td><a href="http://www.community-software-24.com/">community-software-24.com</a></td>
-					<td><a href="https://elgg.org/profile/westor">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Tarun Jangra</td>
-					<td><a href="http://www.pluginlotto.com">http://www.pluginlotto.com</a></td>
-					<td><a href="https://elgg.org/profile/izap">Visit</a></td>
-				</tr>
-				<tr>
-					<td>Shyam Somanadh</td>
-					<td><a href="http://frontiernxt.com">http://frontiernxt.com</a></td>
-					<td><a href="https://elgg.org/profile/codelust">Visit</a></td>
-				</tr>
-				<tr>
-					<td>John L. Spears</td>
-					<td><a rel="nofollow" href="http://www.clothingric.com">http://www.clothingric.com</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>Xavier Vaucois</td>
-					<td><a href="http://www.fairbusinessdeal.com/fbd">http://www.fairbusinessdeal.com/fbd</a></td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>Gordon Wu</td>
-					<td><a href="http://UnixPro.us">http://UnixPro.us</a></td>
-					<td><a href="https://elgg.org/profile/gordonwu888">gordonwu888</a></td>
-				</tr>
-			</tbody>
-		</table>
+		<ul class="elgg-list">
+			<?php
+			foreach ($persons as $person) {
+				?>
+				<li class="elgg-item">
+					<h3><?= $person['name'] ?></h3>
+					<div class="elgg-subtext">
+						<?php
+						echo elgg_view('output/url', [
+							'href' => $person['url'],
+							'is_trusted' => !elgg_extract('nofollow', $person, false),
+							'target' => '_blank',
+						]);
+						?>
+					</div>
+
+					<?php
+					$username = elgg_extract('username', $person);
+					if (!$username) {
+						continue;
+					}
+					$user = get_user_by_username($username);
+					if ($user) {
+						$icon = elgg_view_entity_icon($user, 'topbar');
+						$link = elgg_view('output/url', [
+							'href' => $user->getURL(),
+							'text' => $user->username,
+						]);
+						echo elgg_view_image_block($icon, $link, [
+							'class' => 'elgg-subtext',
+						]);
+					}
+					?>
+				</li>
+				<?php
+			}
+			?>
+		</ul>
 	</div>
 </div>
 
 <div id="disclaimer" class="elgg-box-error">
-	<p><strong>Disclaimer</strong></p>
-	<p>
+	<p class="elgg-output"><strong>Disclaimer</strong></p>
+	<p class="elgg-output">
 		We operate a no refund policy on supporter subscriptions. If you would like to withdraw your support, go to
 		PayPal and cancel your subscription. You will not be billed the following year.</p>
-	<p>
+	<p class="elgg-output">
 		Being an Elgg Supporter does not give an individual or organization the right to impersonate, trade as or
 		imply they are connected to the Elgg project. They can however mention that they support the Elgg
 		project.
 	</p>
-	<p>
+	<p class="elgg-output">
 		If you have any questions about this disclaimer email <a href="mailto:info@elgg.org">info@elgg.org</a>
 	</p>
-	<p>
+	<p class="elgg-output">
 		<small>We reserve the right to remove or refuse a listing without any prior warning at our complete
 			discretion. There is no refund policy.
 		</small>
 	</p>
-	<p>
+	<p class="elgg-output">
 		<small>If there is no obvious use of Elgg, your site will be linked to with "nofollow" set.</small>
 	</p>
 </div>
